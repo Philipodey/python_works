36ad85959e4903c7938a32665eca57ba70bbf67b
Philipodey <philipodey75@gmail.com>
2023-09-21 10:48:36 +0100

    latest commit

--- class_practice/MedianNumber.py
+++ class_practice/MedianNumber.py
@@ -1,9 +1,18 @@
 number = [10, 20, 15, 25, 5, 30, 35, 20, 10, 20]
-arrange = []
+
+for first in range(len(number)):
+    for second in range(first + 1, len(number)):
+        if number[first] > number[second]:
+            number[first], number[second] = number[second], number[first]
+print(number)
+arrange_number = len(number)
+if arrange_number % 2 == 0:
+    first_median = number[arrange_number // 2]
+    second_median = number[arrange_number // 2 - 1]
+    median = (first_median + second_median) / 2
+else:
+    median = number[arrange_number // 2]
+
+print(f"The median is {median}")
 
 
-for row in range(len(number)):
-    for column in range(1 +i, len(number)):
-        if number[row] > number[column]:
-            number[row], number[col]
-    print(arrange)
